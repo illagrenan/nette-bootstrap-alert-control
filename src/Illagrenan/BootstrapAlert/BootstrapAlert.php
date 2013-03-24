3f2acf031295e60c7e683b7527e9350ca2e5dc55
illagrenan <vaclav.dohnal@gmail.com>
2013-03-24 23:59:28 +0100

    Logika tvorby šablon vyčleněna do samostatného package

--- src/Illagrenan/BootstrapAlert/BootstrapAlert.php
+++ src/Illagrenan/BootstrapAlert/BootstrapAlert.php
@@ -2,43 +2,14 @@
 
 namespace Illagrenan\BootstrapAlert;
 
-use Nette\Application\IPresenter;
-use Nette\Application\UI\Control;
-use Nette\Utils\Strings;
+use Illagrenan\BaseControl\BaseControl;
 
 /**
  * @license http://opensource.org/licenses/MIT MIT
  * @author Vašek Dohnal <hello@vaclavdohnal.cz>
  */
-class BootstrapAlert extends Control
+class BootstrapAlert extends BaseControl
 {
-
-    /**
-     * Koncovka latte šablon
-     */
-
-    const LATTE_EXTENSION = ".latte";
-
-    /**
-     * Výchozí cesta úložiště šablon
-     */
-    const DEFAULT_TEMPLATE_PATH = "templates";
-
-    /**
-     * Cesta k šablonám
-     * @var string
-     */
-    private $templatePath;
-
-    /**
-     * @param IPresenter $parent
-     * @param string $name
-     */
-    public function __construct(IPresenter $parent = NULL, $name = NULL)
-    {
-        parent::__construct($parent, $name);
-        $this->templatePath = __DIR__ . "/" . self::DEFAULT_TEMPLATE_PATH . "/";
-    }
 
     /**
      * @param string $content
@@ -52,7 +23,7 @@ class BootstrapAlert extends Control
      */
     public function render($content, $alertHeader = NULL, AlertType $alertType = NULL)
     {
-        $template = $this->createMainTemplate();
+        $template = $this->createTemplateFromName("alert");
 
         if ($alertType == NULL)
         {
@@ -64,31 +35,6 @@ class BootstrapAlert extends Control
         $template->alertType   = $alertType->value();
 
         $template->render();
-    }
-
-    /* ----------------------------------------------
-     * *** TVORBA ŠABLON ***
-     * ---------------------------------------------- */
-
-    private function createMainTemplate()
-    {
-        /* @var $template ITemplate */
-        $template = $this->createTemplate();
-
-        $menuTemplatePath = $this->getTemplatePath("alert");
-        $template->setFile($menuTemplatePath);
-
-        return $template;
-    }
-
-    private function getTemplatePath($templateName)
-    {
-        if (Strings::endsWith($templateName, self::LATTE_EXTENSION) == FALSE)
-        {
-            $templateName = $templateName . self::LATTE_EXTENSION;
-        }
-
-        return $this->templatePath . $templateName;
     }
 
 }
